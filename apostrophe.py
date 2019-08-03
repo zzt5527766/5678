ab9000a5718ad61ebd0a4a697c147f127654517c
zzt5527766 <969067629@qq.com>
2019-08-03 13:23:44 +0800

    first commit

---
+++ apostrophe.py
@@ -0,0 +1,15 @@
+Python 2.7.16 (v2.7.16:413a49145e, Mar  4 2019, 01:37:19) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("\tpython")
+	python
+>>> print("languages:\nPython\nC\nJava")
+languages:
+Python
+C
+Java
+>>> print("languages:\n\tPython\n\tC\n\tJava")
+languages:
+	Python
+	C
+	Java
+>>> 
